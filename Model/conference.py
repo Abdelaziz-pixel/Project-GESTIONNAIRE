0780f16d0511aed801ad4527b405571d71022a38
El Habib <Abdelaziz.59140@gmail.com>
2020-02-11 12:19:54 +0100

    add method create speaker

--- Model/conference.py
+++
@@ -1,8 +0,0 @@
-class Conference():
-
-    def __init__(self):
-        self.titre = None
-        self.resume = None
-        self.date_heure = None
-        self.speaker_id = None
-        
--- Model/speaker.py
+++ Model/speaker.py
@@ -1,8 +1,28 @@
+from Model.connection import *
+
 class Speaker():
 
     def __init__(self):
+        self.choice = connection()
         self.prenom = None
         self.nom = None
         self.description = None
         self.prefession = None
         self.statut = True 
+
+    def create_speaker(self):
+        self.choice.initialize_connection()
+        self.prenom = input("Quel est votre prénom ? ")
+        self.nom = input("Quel est votre nom ? ")
+        self.description = input("Ajoutez une description : ")
+        self.profession = input("Quel est votre prefession ? ")
+        self.choice.cursor.execute("INSERT INTO Speaker(prenom, nom, description, profession) VALUES (%s, %s, %s, %s)",(self.prenom,self.nom,self.description,self.profession))
+        self.choice.connection.commit()
+        self.choice.close_connection()
+
+
+    def delete_speaker(self):
+        pass
+
+    def read_speaker(self):
+        pass
