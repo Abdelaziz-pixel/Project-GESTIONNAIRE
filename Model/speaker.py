58cd04a2270ded411e9e2ad00b62a8c8febc3f20
El Habib <Abdelaziz.59140@gmail.com>
2020-02-11 10:39:56 +0100

    add class Speaker

---
+++ Model/speaker.py
@@ -0,0 +1,8 @@
+class Speaker():
+
+    def __init__(self):
+        self.prenom = None
+        self.nom = None
+        self.description = None
+        self.prefession = None
+        self.statut = True 
