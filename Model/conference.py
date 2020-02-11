441c4cb35628c8f55d1d05e02b49d1ec14f221ed
El Habib <Abdelaziz.59140@gmail.com>
2020-02-11 12:21:11 +0100

    add class and init Conference

---
+++ Model/conference.py
@@ -0,0 +1,16 @@
+class Conference():
+
+    def __init__(self):
+        self.titre = None
+        self.resume = None
+        self.date_heure = None
+        self.speaker_id = None
+
+    def create_conference(self):
+        pass
+
+    def delete_conference(self):
+        pass
+
+    def read_conference(self):
+        pass
