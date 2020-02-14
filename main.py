7ba8a3b79ccb205c26b9407d34cd4bbe4bdf3d01
El Habib <Abdelaziz.59140@gmail.com>
2020-02-14 13:54:29 +0100

    add comment code of file main

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+"""import of Controller class
+    importation de la class Controller"""
 from Controller.controller import *
-
+"""instantiation of the Controller class
+            instantiation de la class Controller"""
 control = Controller()
 control.Choice()
 
