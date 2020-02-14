ea0188a50c7ab43ff9bc7f321ac5edf9ff9c1275
El Habib <Abdelaziz.59140@gmail.com>
2020-02-14 14:19:48 +0100

    add comment code of file speaker

--- Model/speaker.py
+++ Model/speaker.py
@@ -1,12 +1,11 @@
-"""import of the connection from the Model folder
-    importation de la connexion depuis le dossier Model"""
-
+"""import of the Hydrate class 
+    importation de la class Hydrate"""
 from Model.Entities.hydrate import *
     
 class Speaker(Hydrate):
     
-    """Constructor containing the connection and attributes specific to the speaker
-            Constructeur contenant la connexion et les attributs propre au conférencier"""
+    """Constructor containing the attributes specific to the Speaker as well as the hydrate method
+        Constructeur contenant les attributs propre au Speaker ainsi que la méthode hydrate"""
     def __init__(self, data):
         self.speak_id = None
         self.prenom = None
@@ -15,7 +14,8 @@ class Speaker(Hydrate):
         self.statut = None
         self.description = None
         Hydrate.__init__(self,data)
-    
+    """method to return the values in the form of a character string
+        méthode permettant de retourner les valeurs sous forme de chaine de caractére"""
     def __str__(self):
         return """ Id: {}\n Prénom: {}\n Nom: {}\n Profession: {}\n Description: {}\n Statut: {}\n~~~~~~~~~~~~~~~~~~~~~~~~~""".format(self.speak_id, self.prenom, self.nom, self.profession,self.description, self.statut)
     
