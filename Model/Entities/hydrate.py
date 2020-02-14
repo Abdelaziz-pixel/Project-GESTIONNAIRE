c45767b5d7c47184d8abc836408dd53ccf523292
El Habib <Abdelaziz.59140@gmail.com>
2020-02-14 13:58:52 +0100

    add comment code of file hydrate

--- Model/Entities/hydrate.py
+++ Model/Entities/hydrate.py
@@ -1,11 +1,15 @@
+"""import of Conference and Speaker class
+    importation de la class Conference et Speaker"""
 from Model.conference import *
 from Model.speaker import *
 
 class Hydrate():
-
+    """constructor containing the hydrate method
+        constructeur contenant la méthode hydrate"""
     def __init__(self,data):
         self.hydrate(data)
-
+    """hydrate method allowing to recover the value corresponding to the value
+        méthode hydrate permettant de recupérer la valeur correspondant a la valeur"""
     def hydrate(self, data):
         for key, value in data.items():
             if hasattr(self, key):
